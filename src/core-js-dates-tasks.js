0704b0d854fa1df40ee354fe2b698bc3c5ef6e8c
Aleksandr Vavilov <alvi.ua@gmail.com>
2025-03-05 22:18:18 +0300

    feat: getCountDaysInMonth

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -97,8 +97,10 @@ function getNextFriday(date) {
  * 1, 2024 => 31
  * 2, 2024 => 29
  */
-function getCountDaysInMonth(/* month, year */) {
-  throw new Error('Not implemented');
+function getCountDaysInMonth(month, year) {
+  const nextMonth = new Date(year, month, 1);
+  const lastDay = new Date(nextMonth - 1);
+  return lastDay.getDate();
 }
 
 /**
