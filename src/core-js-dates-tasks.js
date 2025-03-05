2f03342680d03d094862677d7b2c52dd3ef87165
Aleksandr Vavilov <alvi.ua@gmail.com>
2025-03-05 22:10:55 +0300

    feat: getNextFriday

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -73,8 +73,17 @@ function getDayName(date) {
  * Date('2024-02-13T00:00:00Z') => Date('2024-02-16T00:00:00Z')
  * Date('2024-02-16T00:00:00Z') => Date('2024-02-23T00:00:00Z')
  */
-function getNextFriday(/* date */) {
-  throw new Error('Not implemented');
+function getNextFriday(date) {
+  const result = new Date(date);
+  const dayIdx = result.getDay();
+  let daysToAdd;
+  if (dayIdx < 5) {
+    daysToAdd = 5 - dayIdx;
+  } else {
+    daysToAdd = 5 + (7 - dayIdx);
+  }
+  result.setDate(result.getDate() + daysToAdd);
+  return result;
 }
 
 /**
