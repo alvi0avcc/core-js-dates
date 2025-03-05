76fb4ddc49eb9b6f62b80daa5917b5e5130203f8
Aleksandr Vavilov <alvi.ua@gmail.com>
2025-03-05 21:24:28 +0300

    feat: getTime

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -31,8 +31,10 @@ function dateToTimestamp(date) {
  * Date(2023, 5, 1, 8, 20, 55) => '08:20:55'
  * Date(2015, 10, 20, 23, 15, 1) => '23:15:01'
  */
-function getTime(/* date */) {
-  throw new Error('Not implemented');
+function getTime(...date) {
+  const myDate = new Date(...date);
+  const addZero = (val) => (val < 10 ? `0${val}` : val);
+  return `${addZero(myDate.getHours())}:${addZero(myDate.getMinutes())}:${addZero(myDate.getSeconds())}`;
 }
 
 /**
